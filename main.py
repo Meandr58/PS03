ce347abccdf115da92f92f77c8b1fcade6a15af3
Meandr58 <andrey.mescherin@gmail.com>
2024-08-03 15:07:57 +0300

    result

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
-
 from bs4 import BeautifulSoup
+from googletrans import Translator
 
 # Создаём функцию, которая будет получать информацию
 def get_english_words():
     url = "https://randomword.com/"
     try:
         response = requests.get(url)
+        response.raise_for_status()  # Проверяем статус ответа ВСТАВКА
 
         # Создаём объект Soup
         soup = BeautifulSoup(response.content, "html.parser")
@@ -15,15 +16,31 @@ def get_english_words():
         # Получаем описание слова
         word_definition = soup.find("div", id="random_word_definition").text.strip()
 
+    # НАЧАЛО ВСТАВКИ
+
+        # Инициализируем переводчик
+        translator = Translator()
+
+        # Переводим слово и его определение на русский язык
+        translated_word = translator.translate(english_words, src='en', dest='ru').text
+        translated_definition = translator.translate(word_definition, src='en', dest='ru').text
+
+    # КОНЕЦ ВСТАВКИ
+
         # Чтобы программа возвращала словарь
         return {
-            "english_words": english_words,
-            "word_definition": word_definition
+            "english_words": translated_word,
+            "word_definition": translated_definition
         }
     # Функция, которая сообщит об ошибке, но не остановит программу
-    except:
-        print("Произошла ошибка")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
+        # Возвращаем пустой словарь в случае ошибки ВСТАВКА
+        return {
+            "english_words": "",
+            "word_definition": ""
+        }
 
 # Создаём функцию, которая будет делать саму игру
 def word_game():
@@ -34,6 +51,11 @@ def word_game():
         word = word_dict.get("english_words")
         word_definition = word_dict.get("word_definition")
 
+        # Проверяем, что слово и определение не пустые ВСТАВКА
+        if not word or not word_definition:
+            print("Не удалось получить слово и его определение. Попробуйте снова.")
+            continue
+
         # Начинаем игру
         print(f"Значение слова - {word_definition}")
         user = input("Что это за слово? ")
@@ -43,7 +65,7 @@ def word_game():
             print(f"Ответ неверный, было загадано это слово - {word}")
 
         # Создаём возможность закончить игру
-        play_again = input("Хотите сыграть еще раз? y/n")
+        play_again = input("Хотите сыграть еще раз? y/n ")
         if play_again != "y":
             print("Спасибо за игру!")
             break
